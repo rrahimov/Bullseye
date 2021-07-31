05eb653121f4bffcfc01312e9e398e7cc4c9e2e1
Ruhullah Rahimov <ruhullah.rahimov@gmail.com>
2021-08-01 01:14:36 +0400

    unit test

---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,50 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Ruhullah Rahimov on 31.07.21.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+    
+    var game : Game!
+
+    override func setUpWithError() throws {
+        game = Game()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        game = nil
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testScorePositive() {
+        var guess = game.target + 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+    func testScoreNegative() {
+        var guess = game.target - 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+    func testExample() throws {
+        XCTAssertEqual(game.points(sliderValue: 50), 9)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
