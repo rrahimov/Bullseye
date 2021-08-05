faabcffe22a821da17b7f6255d6f12fb994d83ef
Ruhullah Rahimov <ruhullah.rahimov@gmail.com>
2021-08-05 21:24:29 +0400

    gradient

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,43 +14,50 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text(String(game.target))
-                .fontWeight(.black)
-                .font(.largeTitle)
-                .kerning(-1.0)
-            HStack {
-                Text("1")
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
                     .bold()
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                //$ makes it binding to the state, which updates it automatically
-                Text("100")
-                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text(String(game.target))
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .kerning(-1.0)
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    //$ makes it binding to the state, which updates it automatically
+                    Text("100")
+                        .bold()
+                }
+                .padding()
+                Button(action: {
+                    print("Hello broseph")
+                    alertIsVisible = true
+                }) {
+                    Text("Hit me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
+                .padding(20.0)
+                .foregroundColor(.white)
+                .background(ZStack {
+                    Color("ButtonColor")
+                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                })
+                .cornerRadius(21.0)
+                
+                .alert(isPresented: $alertIsVisible, content: {
+                    let roundedValue = Int(sliderValue.rounded())
+                    return Alert(title: Text("Hallu"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Coolest thing")))
+                })
             }
-            .padding()
-            Button(action: {
-                print("Hello broseph")
-                alertIsVisible = true
-            }) {
-                Text("Hit me".uppercased())
-                    .bold()
-                    .font(.title3)
-            }
-            .padding(20.0)
-            .foregroundColor(.white)
-            .background(Color.blue)
-            .cornerRadius(21.0)
-            
-            .alert(isPresented: $alertIsVisible, content: {
-                let roundedValue = Int(sliderValue.rounded())
-                return Alert(title: Text("Hallu"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Coolest thing")))
-            })
         }
     }
 }
